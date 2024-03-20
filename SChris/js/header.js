1626cde0aef39ffe9d507cf39c4c3aaca93f3e48
Chris <31750051+xyzchris0@users.noreply.github.com>
2024-03-20 16:32:51 -0500

    Delete SChris directory

--- SChris/js/header.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const headerContainer = document.createElement('div');
-    headerContainer.innerHTML = document.querySelector('script[src*="header.html"]').innerHTML;
-    document.body.insertBefore(headerContainer, document.body.firstChild);
-});
